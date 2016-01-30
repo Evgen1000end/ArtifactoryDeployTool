4c0467fe7c28c8a7d3a05973cfc2b6c83f7a254d
evgen1000end <evgen1000end@gmail.com>
2016-01-30 11:13:17 +0300

    add code from first version

---
+++ src/main/java/ru/demkin/simplepublisher/SimpleArtefactoryPublisher.java
@@ -0,0 +1,14 @@
+package ru.demkin.simplepublisher;
+
+import org.jfrog.artifactory.api.ArtifactoryAPI;
+import org.jfrog.artifactory.api.impl.ArtifactoryAPIImpl;
+
+import java.io.File;
+
+public class SimpleArtefactoryPublisher {
+
+    public static void main(String[] args) {
+        ArtifactoryAPI artifactoryAPI = new ArtifactoryAPIImpl(new DefautParameterBuilder());
+        artifactoryAPI.importFrom(new File("C:\\NEW_WORKSPACE\\CZML.zip"));
+    }
+}
